bbad6270d86d1c153cbaff9eb93b5ed9bfc30770
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2010-12-08 14:22:34 +0000

    Ajax controller for people base.
    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@57 3b25252b-017b-4ae5-8f61-fa87a855a72e

---
+++ DocSources/src/main/java/org/medici/docsources/controller/peoplebase/AjaxController.java
@@ -0,0 +1,97 @@
+/*
+ * AjaxController.java
+ * 
+ * Developed by Medici Archive Project (2010-2012).
+ * 
+ * This file is part of DocSources.
+ * 
+ * DocSources is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * DocSources is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, if you link this library with other files to
+ * produce an executable, this library does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * This exception does not however invalidate any other reasons why the
+ * executable file might be covered by the GNU General Public License.
+ */
+package org.medici.docsources.controller.peoplebase;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.medici.docsources.common.util.ListBeanUtils;
+import org.medici.docsources.domain.People;
+import org.medici.docsources.domain.SerieList;
+import org.medici.docsources.exception.ApplicationThrowable;
+import org.medici.docsources.service.peoplebase.PeopleBaseService;
+import org.medici.docsources.service.user.UserService;
+import org.medici.docsources.service.volbase.VolBaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * AJAX Controller for PeopleBase.
+ * 
+ * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
+ */
+@Controller("PeoleBaseAjaxController")
+public class AjaxController {
+	@Autowired
+	private PeopleBaseService peopleBaseService;
+
+	/**
+	 * This method returns a list of ipotetical senders. 
+	 *  
+	 * @param text Text to search in ...
+	 * @return ModelAndView containing senders.
+	 */
+	@RequestMapping(value = "/de/peoplebase/FindSenders", method = RequestMethod.GET)
+	public ModelAndView findSeriesList(@RequestParam("query") String query) {
+		Map<String, Object> model = new HashMap<String, Object>();
+
+		try {
+			//<!-- Autocomplete (SELECT [tblPeople].[MAPnameLF], [tblPeople].[ACTIVESTART], [tblPeople].[BYEAR], [tblPeople].[DYEAR] FROM tblPeople ORDER BY [MAPnameLF];) -->
+
+			List<People> people = getPeopleBaseService().findSenders(query);
+			model.put("query", query);
+			//model.put("data", ListBeanUtils.transformList(series, "seriesRefNum"));
+			//model.put("suggestions", ListBeanUtils.toStringListWithConcatenationFields(series, "title/subTitle1/subTitle2", "/", "/", Boolean.TRUE));
+
+		} catch (ApplicationThrowable aex) {
+			return new ModelAndView("responseKO", model);
+		}
+
+		return new ModelAndView("responseOK", model);
+	}
+
+	/**
+	 * @param peopleBaseService the peopleBaseService to set
+	 */
+	public void setPeopleBaseService(PeopleBaseService peopleBaseService) {
+		this.peopleBaseService = peopleBaseService;
+	}
+
+	/**
+	 * @return the peopleBaseService
+	 */
+	public PeopleBaseService getPeopleBaseService() {
+		return peopleBaseService;
+	}
+
+}
