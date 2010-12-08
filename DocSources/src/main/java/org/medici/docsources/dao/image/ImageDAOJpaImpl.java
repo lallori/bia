b4590aa794722d6f3fba8851d872f06b170177f2
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2010-12-08 14:18:56 +0000

    JPA DAO implementation for image entity.
    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@54 3b25252b-017b-4ae5-8f61-fa87a855a72e

---
+++ DocSources/src/main/java/org/medici/docsources/dao/image/ImageDAOJpaImpl.java
@@ -0,0 +1,104 @@
+/*
+ * ImageDAOJpaImpl.java
+ * 
+ * Developed by Medici Archive Project (2010-2012).
+ * 
+ * This file is part of DocSources.
+ * 
+ * DocSources is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * DocSources is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, if you link this library with other files to
+ * produce an executable, this library does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * This exception does not however invalidate any other reasons why the
+ * executable file might be covered by the GNU General Public License.
+ */
+package org.medici.docsources.dao.image;
+
+import java.util.List;
+
+import javax.persistence.PersistenceException;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.ParameterExpression;
+import javax.persistence.criteria.Root;
+
+import org.apache.commons.lang.StringUtils;
+import org.medici.docsources.dao.JpaDao;
+import org.medici.docsources.domain.Image;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <b>ImageDAOJpaImpl</b> is a default implementation of
+ * <b>ImageDAO</b>.
+ * 
+ * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
+ * 
+ * @see org.medici.docsources.domain.HistoryLog
+ * @see org.medici.docsources.audit.HistoryLogAction
+ */
+@Repository
+public class ImageDAOJpaImpl extends JpaDao<Integer, Image> implements ImageDAO {
+
+	/**
+	 * 
+	 *  If a serializable class does not explicitly declare a serialVersionUID, 
+	 *  then the serialization runtime will calculate a default serialVersionUID 
+	 *  value for that class based on various aspects of the class, as described
+	 *  in the Java(TM) Object Serialization Specification. However, it is 
+	 *  strongly recommended that all serializable classes explicitly declare 
+	 *  serialVersionUID values, since the default serialVersionUID computation 
+	 *  is highly sensitive to class details that may vary depending on compiler
+	 *  implementations, and can thus result in unexpected 
+	 *  InvalidClassExceptions during deserialization. Therefore, to guarantee a
+	 *   consistent serialVersionUID value across different java compiler 
+	 *   implementations, a serializable class must declare an explicit 
+	 *  serialVersionUID value. It is also strongly advised that explicit 
+	 *  serialVersionUID declarations use the private modifier where possible, 
+	 *  since such declarations apply only to the immediately declaring 
+	 *  class--serialVersionUID fields are not useful as inherited members. 
+	 */
+	private static final long serialVersionUID = -8769762056162920397L;
+
+	@Override
+	public List<Image> findImages(Integer volNum, String volLetExt) throws PersistenceException {
+		// Create criteria objects
+		CriteriaBuilder criteriaBuilder = getEntityManager().getCriteriaBuilder();
+		CriteriaQuery<Image> criteriaQuery = criteriaBuilder.createQuery(Image.class);
+		Root<Image> root = criteriaQuery.from(Image.class);
+	
+		// Define predicate's elements
+		ParameterExpression<Integer> parameterVolNum = criteriaBuilder.parameter(Integer.class, "volNum");
+		ParameterExpression<String> parameterVolLeText = StringUtils.isEmpty("volLetExt") ? null : criteriaBuilder.parameter(String.class, "volLetExt"); 
+
+		criteriaQuery.where(
+			criteriaBuilder.and(
+				criteriaBuilder.equal(root.get("volNum"), parameterVolNum),
+				StringUtils.isEmpty(volLetExt) ? 
+					criteriaBuilder.isNull(root.get("volLetExt")) : 
+					criteriaBuilder.equal(root.get("volLetExt"), parameterVolLeText)
+			)
+		);
+
+		// Set values in predicate's elements  
+		TypedQuery<Image> typedQuery = getEntityManager().createQuery(criteriaQuery);
+		typedQuery.setParameter("volNum", volNum);
+		if (!StringUtils.isEmpty(volLetExt))
+			typedQuery.setParameter("volLetExt", volLetExt);
+
+		return typedQuery.getResultList();
+	}
+}
