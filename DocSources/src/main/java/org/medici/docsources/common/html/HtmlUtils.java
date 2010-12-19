8a72b21c6cd6105060baab63726a0a60d822f4a8
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2010-12-19 16:42:01 +0000

    New class to generate html tags for application.
    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@122 3b25252b-017b-4ae5-8f61-fa87a855a72e

---
+++ DocSources/src/main/java/org/medici/docsources/common/html/HtmlUtils.java
@@ -0,0 +1,122 @@
+/*
+ * HtmlUtils.java
+ *
+ * Developed by The Medici Archive Project Inc. (2010-2012)
+ * 
+ * This file is part of DocSources.
+ * 
+ * DocSources is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * DocSources is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, if you link this library with other files to
+ * produce an executable, this library does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * This exception does not however invalidate any other reasons why the
+ * executable file might be covered by the GNU General Public License.
+ */
+package org.medici.docsources.common.html;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.html.dom.HTMLAnchorElementImpl;
+import org.apache.html.dom.HTMLDocumentImpl;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+/**
+ * 
+ * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
+ *
+ */
+public class HtmlUtils {
+
+	public static List<String> showDocument(List<String> inputList, Integer entryId) {
+		if (inputList == null)
+			return null;
+
+		ArrayList<String> retValue = new ArrayList<String>(inputList.size());
+		
+		StringBuffer anchorBegin = new StringBuffer("<a href=\"");
+		anchorBegin.append(((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest().getContextPath());
+		anchorBegin.append("/src/docbase/ShowDocument.do?entryId=");
+		anchorBegin.append(entryId);
+		anchorBegin.append("\">");
+		String hrefEnd = "</a>";
+		HTMLDocumentImpl htmlDocumentImpl = new HTMLDocumentImpl();
+		HTMLAnchorElementImpl htmlAnchorElementImpl = new HTMLAnchorElementImpl(htmlDocumentImpl, "showVolume");
+		htmlAnchorElementImpl.setHref(anchorBegin.toString());
+		
+		for (int i=0; i<inputList.size(); i++) {
+			StringBuffer stringBuffer = new StringBuffer(anchorBegin.toString());
+			stringBuffer.append(inputList.get(i));
+			stringBuffer.append(hrefEnd);
+			retValue.add(stringBuffer.toString());
+		}
+		
+		return retValue;
+	}
+
+	public static List<String> showPeople(List<String> inputList, Integer entryId) {
+		if (inputList == null)
+			return null;
+
+		ArrayList<String> retValue = new ArrayList<String>(inputList.size());
+		
+		StringBuffer anchorBegin = new StringBuffer("<a href=\"");
+		anchorBegin.append(((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest().getContextPath());
+		anchorBegin.append("/src/peoplebase/ShowDocument.do?entryId=");
+		anchorBegin.append(entryId);
+		anchorBegin.append("\">");
+		String hrefEnd = "</a>";
+		HTMLDocumentImpl htmlDocumentImpl = new HTMLDocumentImpl();
+		HTMLAnchorElementImpl htmlAnchorElementImpl = new HTMLAnchorElementImpl(htmlDocumentImpl, "showVolume");
+		htmlAnchorElementImpl.setHref(anchorBegin.toString());
+		
+		for (int i=0; i<inputList.size(); i++) {
+			StringBuffer stringBuffer = new StringBuffer(anchorBegin.toString());
+			stringBuffer.append(inputList.get(i));
+			stringBuffer.append(hrefEnd);
+			retValue.add(stringBuffer.toString());
+		}
+		
+		return retValue;
+	}
+
+	public static List<String> showVolume(List<String> inputList, Integer summaryId) {
+		if (inputList == null)
+			return null;
+
+		ArrayList<String> retValue = new ArrayList<String>(inputList.size());
+		
+		StringBuffer anchorBegin = new StringBuffer("<a href=\"");
+		anchorBegin.append(((ServletRequestAttributes)RequestContextHolder.currentRequestAttributes()).getRequest().getContextPath());
+		anchorBegin.append("/src/volbase/ShowVolume.do?summaryId=");
+		anchorBegin.append(summaryId);
+		anchorBegin.append("\">");
+		String hrefEnd = "</a>";
+		HTMLDocumentImpl htmlDocumentImpl = new HTMLDocumentImpl();
+		HTMLAnchorElementImpl htmlAnchorElementImpl = new HTMLAnchorElementImpl(htmlDocumentImpl, "showVolume");
+		htmlAnchorElementImpl.setHref(anchorBegin.toString());
+		
+		for (int i=0; i<inputList.size(); i++) {
+			StringBuffer stringBuffer = new StringBuffer(anchorBegin.toString());
+			stringBuffer.append(inputList.get(i));
+			stringBuffer.append(hrefEnd);
+			retValue.add(stringBuffer.toString());
+		}
+		
+		return retValue;
+	}
+}
