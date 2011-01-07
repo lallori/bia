433085bb231512cb9d6ce024ee3d20d8c605ed28
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2011-01-07 09:42:02 +0000

    Controller for action edit research notes.
    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@230 3b25252b-017b-4ae5-8f61-fa87a855a72e

---
+++ DocSources/src/main/java/org/medici/docsources/controller/peoplebase/EditResearchNotesPersonController.java
@@ -0,0 +1,126 @@
+/*
+ * EditResearchNotesPersonController.java
+ * 
+ * Developed by Medici Archive Project (2010-2012).
+ * 
+ * This file is part of DocSources.
+ * 
+ * DocSources is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * DocSources is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, if you link this library with other files to
+ * produce an executable, this library does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * This exception does not however invalidate any other reasons why the
+ * executable file might be covered by the GNU General Public License.
+ */
+package org.medici.docsources.controller.peoplebase;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.validation.Valid;
+
+import org.medici.docsources.command.peoplebase.EditResearchNotesPersonCommand;
+import org.medici.docsources.service.peoplebase.PeopleBaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.Validator;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Controller for action "Edit Research Notes of a Person".
+ * 
+ * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
+ */
+@Controller
+@RequestMapping("/de/peoplebase/EditResearchNotesPerson")
+public class EditResearchNotesPersonController {
+	@Autowired
+	private PeopleBaseService peopleBaseService;
+	@Autowired(required = false)
+	@Qualifier("modifyPersonValidator")
+	private Validator validator;
+
+	/**
+	 * @return the peopleBaseService
+	 */
+	public PeopleBaseService getPeopleBaseService() {
+		return peopleBaseService;
+	}
+
+	/**
+	 * This method returns the Validator class used by Controller to make
+	 * business validation.
+	 * 
+	 * @return
+	 */
+	public Validator getValidator() {
+		return validator;
+	}
+
+	/**
+	 * 
+	 * @param command
+	 * @param result
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.POST)
+	public ModelAndView processSubmit(@Valid @ModelAttribute("command") EditResearchNotesPersonCommand command, BindingResult result) {
+		getValidator().validate(command, result);
+
+		if (result.hasErrors()) {
+			return setupForm(command);
+		} else {
+			Map<String, Object> model = new HashMap<String, Object>();
+
+			/** TODO : Implement invocation business logic */
+			getPeopleBaseService();
+
+			return new ModelAndView("peoplebase/modifyperson", model);
+		}
+
+	}
+
+	/**
+	 * @param peopleBaseService
+	 *            the peopleBaseService to set
+	 */
+	public void setPeopleBaseService(PeopleBaseService peopleBaseService) {
+		this.peopleBaseService = peopleBaseService;
+	}
+
+	/**
+	 * 
+	 * @param command
+	 * @return
+	 */
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView setupForm(@ModelAttribute("command") EditResearchNotesPersonCommand command) {
+		return new ModelAndView("peoplebase/modifyperson");
+	}
+
+	/**
+	 * 
+	 * @param validator
+	 */
+	public void setValidator(Validator validator) {
+		this.validator = validator;
+	}
+}
