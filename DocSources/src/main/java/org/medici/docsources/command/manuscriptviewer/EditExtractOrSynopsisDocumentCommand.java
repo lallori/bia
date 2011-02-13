cc5600edee1adcdeb219a4d16603132d5db18a36
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2011-02-13 20:27:20 +0000

    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@326 3b25252b-017b-4ae5-8f61-fa87a855a72e

---
+++ DocSources/src/main/java/org/medici/docsources/command/manuscriptviewer/EditExtractOrSynopsisDocumentCommand.java
@@ -0,0 +1,118 @@
+/*
+ * EditExtractOrSynopsisDocumentCommand.java
+ *
+ * Developed by The Medici Archive Project Inc. (2010-2012)
+ * 
+ * This file is part of DocSources.
+ * 
+ * DocSources is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * DocSources is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, if you link this library with other files to
+ * produce an executable, this library does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * This exception does not however invalidate any other reasons why the
+ * executable file might be covered by the GNU General Public License.
+ */
+package org.medici.docsources.command.manuscriptviewer;
+
+
+/**
+ * Command bean for action "Edit Extract Or Synopsis Document".
+ * 
+ * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
+ * 
+ * @see org.medici.docsources.controller.docbase.EditExtractOrSynopsisDocumentController
+ */
+public class EditExtractOrSynopsisDocumentCommand {
+	private Integer synExtrId;
+	private Integer entryId;
+	private String docExtract;
+	private String synopsis;
+	private Boolean manuscriptViewer;
+
+	/**
+	 * This method returns entryId property.
+	 * 
+	 * @return the entryId
+	 */
+	public Integer getEntryId() {
+		return entryId;
+	}
+
+	/**
+	 * This method sets document unique id property.
+	 * 
+	 * @param the entryId to set
+	 */
+	public void setEntryId(Integer entryId) {
+		this.entryId = entryId;
+	}
+
+	/**
+	 * @param docExtract the docExtract to set
+	 */
+	public void setDocExtract(String docExtract) {
+		this.docExtract = docExtract;
+	}
+
+	/**
+	 * @return the docExtract
+	 */
+	public String getDocExtract() {
+		return docExtract;
+	}
+
+	/**
+	 * @param manuscriptViewer the manuscriptViewer to set
+	 */
+	public void setManuscriptViewer(Boolean manuscriptViewer) {
+		this.manuscriptViewer = manuscriptViewer;
+	}
+
+	/**
+	 * @return the manuscriptViewer
+	 */
+	public Boolean getManuscriptViewer() {
+		return manuscriptViewer;
+	}
+
+	/**
+	 * @param synopsis the synopsis to set
+	 */
+	public void setSynopsis(String synopsis) {
+		this.synopsis = synopsis;
+	}
+
+	/**
+	 * @return the synopsis
+	 */
+	public String getSynopsis() {
+		return synopsis;
+	}
+
+	/**
+	 * @param synExtrId the synExtrId to set
+	 */
+	public void setSynExtrId(Integer synExtrId) {
+		this.synExtrId = synExtrId;
+	}
+
+	/**
+	 * @return the synExtrId
+	 */
+	public Integer getSynExtrId() {
+		return synExtrId;
+	}
+}
