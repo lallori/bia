baeec98656290ee328bf3a2e686a2330bf3cf29e
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2011-02-19 12:15:15 +0000

    Removed unused import class.
    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@381 3b25252b-017b-4ae5-8f61-fa87a855a72e

--- DocSources/src/main/java/org/medici/docsources/domain/People.java
+++ DocSources/src/main/java/org/medici/docsources/domain/People.java
@@ -44,7 +44,6 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
-import javax.persistence.OrderColumn;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
