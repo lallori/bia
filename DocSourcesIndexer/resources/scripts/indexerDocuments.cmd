7900b527f5a208ca2b4d4e4f1c59758373755b4d
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2011-04-10 09:42:28 +0000

    Scripts to run indexer.
    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@640 3b25252b-017b-4ae5-8f61-fa87a855a72e

---
+++ DocSourcesIndexer/resources/scripts/indexerDocuments.cmd
@@ -0,0 +1,4 @@
+set JAVA_HOME="C:\Program Files\Java\jdk1.6.0_20"
+set JAVA_OPTS="-Xms128m -Xmx1024"
+
+%JAVA_HOME%\bin\java.exe -classpath ".\*;.\libs\*;" org.medici.docsources.indexer.DocSourcesIndexer documents
---
+++ DocSourcesIndexer/resources/scripts/indexerPeople.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk
+export JAVA_OPTS="-Xms128m -Xmx1024"
+
+java -classpath './*:./libs/*' org.medici.docsources.indexer.DocSourcesIndexer people
+
