b555b705bc72045c9a26d5ff6ac6ed2de88b867b
matteod <matteod@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2011-07-25 14:57:50 +0000

    Fixed a control for match the searchType
    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@988 3b25252b-017b-4ae5-8f61-fa87a855a72e

--- DocSources/src/main/java/org/medici/docsources/controller/search/AdvancedSearchController.java
+++ DocSources/src/main/java/org/medici/docsources/controller/search/AdvancedSearchController.java
@@ -146,9 +146,9 @@ public class AdvancedSearchController {
 		
 		if (searchFilter.getSearchType().equals(SearchType.DOCUMENT)) {
 			return new ModelAndView("search/AdvancedSearchDocuments", model);
-		} else if (searchFilter.equals(SearchType.PEOPLE)) {
+		} else if (searchFilter.getSearchType().equals(SearchType.PEOPLE)) {
 			return new ModelAndView("search/AdvancedSearchPeople", model);
-		} else if (searchFilter.equals(SearchType.PLACE)) {
+		} else if (searchFilter.getSearchType().equals(SearchType.PLACE)) {
 			return new ModelAndView("search/AdvancedSearchPlaces", model);
 		} else {
 			return new ModelAndView("search/AdvancedSearchVolumes", model);
