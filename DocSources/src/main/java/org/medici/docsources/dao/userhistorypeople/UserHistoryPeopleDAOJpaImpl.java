6563df950a6a6309d1d63b49911d86e22f17ac00
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2011-11-20 15:51:10 +0000

    New dao.
    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@1166 3b25252b-017b-4ae5-8f61-fa87a855a72e

---
+++ DocSources/src/main/java/org/medici/docsources/dao/userhistorypeople/UserHistoryPeopleDAOJpaImpl.java
@@ -0,0 +1,137 @@
+/*
+ * UserHistoryPeopleDAOJpaImpl.java
+ * 
+ * Developed by Medici Archive Project (2010-2012).
+ * 
+ * This file is part of DocSources.
+ * 
+ * DocSources is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * DocSources is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, if you link this library with other files to
+ * produce an executable, this library does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * This exception does not however invalidate any other reasons why the
+ * executable file might be covered by the GNU General Public License.
+ */
+package org.medici.docsources.dao.userhistorypeople;
+
+import java.util.List;
+
+import javax.persistence.PersistenceException;
+import javax.persistence.Query;
+import javax.persistence.criteria.CriteriaBuilder;
+
+import org.medici.docsources.common.pagination.Page;
+import org.medici.docsources.common.pagination.PaginationFilter;
+import org.medici.docsources.dao.JpaDao;
+import org.medici.docsources.domain.UserHistoryPeople;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <b>UserHistoryPeopleDAOJpaImpl</b> is a default implementation of
+ * <b>UserHistoryPeopleDAO</b>.
+ * 
+ * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
+ * 
+ * @see org.medici.docsources.domain.UserHistory
+ */
+@Repository
+public class UserHistoryPeopleDAOJpaImpl extends JpaDao<Integer, UserHistoryPeople> implements UserHistoryPeopleDAO {
+
+	/**
+	 * 
+	 *  If a serializable class does not explicitly declare a serialVersionUID, 
+	 *  then the serialization runtime will calculate a default serialVersionUID 
+	 *  value for that class based on various aspects of the class, as described
+	 *  in the Java(TM) Object Serialization Specification. However, it is 
+	 *  strongly recommended that all serializable classes explicitly declare 
+	 *  serialVersionUID values, since the default serialVersionUID computation 
+	 *  is highly sensitive to class details that may vary depending on compiler
+	 *  implementations, and can thus result in unexpected 
+	 *  InvalidClassExceptions during deserialization. Therefore, to guarantee a
+	 *   consistent serialVersionUID value across different java compiler 
+	 *   implementations, a serializable class must declare an explicit 
+	 *  serialVersionUID value. It is also strongly advised that explicit 
+	 *  serialVersionUID declarations use the private modifier where possible, 
+	 *  since such declarations apply only to the immediately declaring 
+	 *  class--serialVersionUID fields are not useful as inherited members. 
+	 */
+	private static final long serialVersionUID = 8495652438368426749L;
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public List<UserHistoryPeople> findHistory(Integer resultSize) {
+        String queryString = "FROM UserHistoryDocument WHERE username=:username ORDER BY dateAndTime DESC";
+
+        Query query = getEntityManager().createQuery(queryString);
+
+        query.setParameter("username", ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername());
+        query.setMaxResults(resultSize);
+        
+		return query.getResultList();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public UserHistoryPeople findLastEntryPeople() {
+        String queryString = "FROM UserHistoryPeople WHERE username=:username ORDER BY dateAndTime DESC";
+
+        Query query = getEntityManager().createQuery(queryString);
+
+        query.setParameter("username", ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername());
+        query.setMaxResults(1);
+
+		List<UserHistoryPeople> result = query.getResultList();
+		
+		if (result.size() == 1) {
+			return result.get(0);
+		} 
+
+		return null;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public Page findHistory(PaginationFilter paginationFilter) throws PersistenceException {
+		Page page = new Page(paginationFilter);
+
+		if (paginationFilter.getTotal() == null) {
+	        String queryString = "SELECT count(1) FROM UserHistoryPeople WHERE username=:username ORDER BY dateAndTime DESC";
+
+	        Query query = getEntityManager().createQuery(queryString);
+	        query.setParameter("username", ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername());
+			page.setTotal(new Long((Long)query.getSingleResult()));
+		}
+
+        String queryString = "FROM UserHistoryPeople WHERE username=:username ORDER BY dateAndTime DESC";
+
+        Query query = getEntityManager().createQuery(queryString);
+        query.setParameter("username", ((UserDetails) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUsername());
+		query.setFirstResult(paginationFilter.getFirstRecord());
+		query.setMaxResults(paginationFilter.getLength());
+		page.setList(query.getResultList());
+
+		return page;
+	}
+
+}
