ee4cb7d5b026637c66dd539e593ebbe82a1b8377
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2011-11-20 09:47:48 +0000

    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@1157 3b25252b-017b-4ae5-8f61-fa87a855a72e

--- DocSourcesIndexer/resources/scripts/indexerDocuments.cmd
+++ DocSourcesIndexer/resources/scripts/indexerDocuments.cmd
@@ -1,4 +1,4 @@
 set JAVA_HOME="C:\Program Files\Java\jdk1.6.0_20"
-set JAVA_OPTS="-Xm512m -Xmx1536m"
+set JAVA_OPTS="-Xm512m -Xmx2036m"
 
 %JAVA_HOME%\bin\java.exe -classpath ".\*;.\libs\*;" org.medici.docsources.indexer.DocSourcesIndexer documents
