1e99df5e802cfeb59ddf8f1d38cbb65bfa54ea3f
matteod <matteod@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2011-11-30 16:02:32 +0000

    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@1201 3b25252b-017b-4ae5-8f61-fa87a855a72e

--- DocSources/src/main/java/org/medici/docsources/command/search/AdvancedSearchPeopleCommand.java
+++ DocSources/src/main/java/org/medici/docsources/command/search/AdvancedSearchPeopleCommand.java
@@ -29,6 +29,8 @@ package org.medici.docsources.command.search;
 
 import java.util.List;
 
+import org.medici.docsources.common.search.AdvancedSearchAbstract.NameType;
+import org.medici.docsources.common.search.AdvancedSearchAbstract.WordType;
 import org.medici.docsources.domain.SearchFilter.SearchType;
 
 /**
@@ -39,7 +41,214 @@ import org.medici.docsources.domain.SearchFilter.SearchType;
 public class AdvancedSearchPeopleCommand {
 	private Integer idSearchFilter;
 	private SearchType searchType;
-	private List<String> namePerson;
+	/**
+	 * @return the namesTypes
+	 */
+	public List<NameType> getNamesTypes() {
+		return namesTypes;
+	}
+	/**
+	 * @param namesTypes the namesTypes to set
+	 */
+	public void setNamesTypes(List<NameType> namesTypes) {
+		this.namesTypes = namesTypes;
+	}
+	/**
+	 * @return the names
+	 */
+	public List<String> getNames() {
+		return names;
+	}
+	/**
+	 * @param names the names to set
+	 */
+	public void setNames(List<String> names) {
+		this.names = names;
+	}
+	/**
+	 * @return the wordsTypes
+	 */
+	public List<WordType> getWordsTypes() {
+		return wordsTypes;
+	}
+	/**
+	 * @param wordsTypes the wordsTypes to set
+	 */
+	public void setWordsTypes(List<WordType> wordsTypes) {
+		this.wordsTypes = wordsTypes;
+	}
+	/**
+	 * @return the words
+	 */
+	public List<String> getWords() {
+		return words;
+	}
+	/**
+	 * @param words the words to set
+	 */
+	public void setWords(List<String> words) {
+		this.words = words;
+	}
+	/**
+	 * @return the datesDay
+	 */
+	public List<Integer> getDatesDay() {
+		return datesDay;
+	}
+	/**
+	 * @param datesDay the datesDay to set
+	 */
+	public void setDatesDay(List<Integer> datesDay) {
+		this.datesDay = datesDay;
+	}
+	/**
+	 * @return the datesMonth
+	 */
+	public List<Integer> getDatesMonth() {
+		return datesMonth;
+	}
+	/**
+	 * @param datesMonth the datesMonth to set
+	 */
+	public void setDatesMonth(List<Integer> datesMonth) {
+		this.datesMonth = datesMonth;
+	}
+	/**
+	 * @return the datesTypes
+	 */
+	public List<String> getDatesTypes() {
+		return datesTypes;
+	}
+	/**
+	 * @param datesTypes the datesTypes to set
+	 */
+	public void setDatesTypes(List<String> datesTypes) {
+		this.datesTypes = datesTypes;
+	}
+	/**
+	 * @return the datesYear
+	 */
+	public List<Integer> getDatesYear() {
+		return datesYear;
+	}
+	/**
+	 * @param datesYear the datesYear to set
+	 */
+	public void setDatesYear(List<Integer> datesYear) {
+		this.datesYear = datesYear;
+	}
+	/**
+	 * @return the datesYearBetween
+	 */
+	public List<Integer> getDatesYearBetween() {
+		return datesYearBetween;
+	}
+	/**
+	 * @param datesYearBetween the datesYearBetween to set
+	 */
+	public void setDatesYearBetween(List<Integer> datesYearBetween) {
+		this.datesYearBetween = datesYearBetween;
+	}
+	/**
+	 * @return the datesMonthBetween
+	 */
+	public List<Integer> getDatesMonthBetween() {
+		return datesMonthBetween;
+	}
+	/**
+	 * @param datesMonthBetween the datesMonthBetween to set
+	 */
+	public void setDatesMonthBetween(List<Integer> datesMonthBetween) {
+		this.datesMonthBetween = datesMonthBetween;
+	}
+	/**
+	 * @return the datesDayBetween
+	 */
+	public List<Integer> getDatesDayBetween() {
+		return datesDayBetween;
+	}
+	/**
+	 * @param datesDayBetween the datesDayBetween to set
+	 */
+	public void setDatesDayBetween(List<Integer> datesDayBetween) {
+		this.datesDayBetween = datesDayBetween;
+	}
+	/**
+	 * @return the placeId
+	 */
+	public List<Integer> getPlaceId() {
+		return placeId;
+	}
+	/**
+	 * @param placeId the placeId to set
+	 */
+	public void setPlaceId(List<Integer> placeId) {
+		this.placeId = placeId;
+	}
+	/**
+	 * @return the place
+	 */
+	public List<String> getPlace() {
+		return place;
+	}
+	/**
+	 * @param place the place to set
+	 */
+	public void setPlace(List<String> place) {
+		this.place = place;
+	}
+	/**
+	 * @return the roleCategories
+	 */
+	public List<String> getRoleCategories() {
+		return roleCategories;
+	}
+	/**
+	 * @param roleCategories the roleCategories to set
+	 */
+	public void setRoleCategories(List<String> roleCategories) {
+		this.roleCategories = roleCategories;
+	}
+	/**
+	 * @return the titlesOcc
+	 */
+	public List<String> getTitlesOcc() {
+		return titlesOcc;
+	}
+	/**
+	 * @param titlesOcc the titlesOcc to set
+	 */
+	public void setTitlesOcc(List<String> titlesOcc) {
+		this.titlesOcc = titlesOcc;
+	}
+	/**
+	 * @return the titlesOccId
+	 */
+	public List<Integer> getTitlesOccId() {
+		return titlesOccId;
+	}
+	/**
+	 * @param titlesOccId the titlesOccId to set
+	 */
+	public void setTitlesOccId(List<Integer> titlesOccId) {
+		this.titlesOccId = titlesOccId;
+	}
+	private List<NameType> namesTypes;
+	private List<String> names;
+	private List<WordType> wordsTypes;
+	private List<String> words;
+	private List<Integer> datesDay;
+	private List<Integer> datesMonth;
+	private List<String> datesTypes;
+	private List<Integer> datesYear;
+	private List<Integer> datesYearBetween;
+	private List<Integer> datesMonthBetween;
+	private List<Integer> datesDayBetween;
+	private List<Integer> placeId;
+	private List<String> place;
+	private List<String> roleCategories;
+	private List<String> titlesOcc;
+	private List<Integer> titlesOccId;
 	
 	/**
 	 * @param idSearchFilter the idSearchFilter to set
@@ -65,17 +274,6 @@ public class AdvancedSearchPeopleCommand {
 	public SearchType getSearchType() {
 		return searchType;
 	}
-	/**
-	 * @param word the word to set
-	 */
-	public void setNamePerson(List<String> namePerson) {
-		this.namePerson = namePerson;
-	}
-	/**
-	 * @return the word
-	 */
-	public List<String> getNamePerson() {
-		return namePerson;
-	}
+	
 
 }
