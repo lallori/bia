c3ac3786c1eaf0c64c13585bf849369bc32ce682
matteod <matteod@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2012-01-16 15:30:29 +0000

    Small changes for fix bugs.
    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@1382 3b25252b-017b-4ae5-8f61-fa87a855a72e

--- DocSources/src/main/java/org/medici/docsources/common/search/AdvancedSearchPeople.java
+++ DocSources/src/main/java/org/medici/docsources/common/search/AdvancedSearchPeople.java
@@ -764,7 +764,6 @@ public class AdvancedSearchPeople extends AdvancedSearchAbstract {
 //					}catch(Exception e){
 //						
 //					}
-					subQuery.add(new TermQuery(new Term("mapNameLf", names.get(i).toString().toLowerCase())),Occur.MUST);
 					subQuery.add(new TermQuery(new Term("altName.nameType", namesTypes.get(i).toString().toLowerCase())), Occur.MUST);
 					subQuery.add(new TermQuery(new Term("altName.altName", names.get(i).toLowerCase())), Occur.MUST);
 					namesQuery.add(subQuery, Occur.MUST);
