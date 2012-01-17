1d7904c88c9f720da9180b5e10a651e6f4d42cb6
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2012-01-17 22:35:08 +0000

    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@1395 3b25252b-017b-4ae5-8f61-fa87a855a72e

--- DocSources/src/main/java/org/medici/docsources/common/pagination/PaginationFilter.java
+++ DocSources/src/main/java/org/medici/docsources/common/pagination/PaginationFilter.java
@@ -1,201 +1,236 @@
-/*
- * PaginationFilter.java
- *
- * Developed by The Medici Archive Project Inc. (2010-2012)
- * 
- * This file is part of DocSources.
- * 
- * DocSources is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * DocSources is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * As a special exception, if you link this library with other files to
- * produce an executable, this library does not by itself cause the
- * resulting executable to be covered by the GNU General Public License.
- * This exception does not however invalidate any other reasons why the
- * executable file might be covered by the GNU General Public License.
- */
-package org.medici.docsources.common.pagination;
+ package org.medici.docsources.common.pagination;
 
-import java.util.ArrayList;
+ import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.commons.lang.ObjectUtils;
 import org.apache.lucene.search.SortField;
+import org.medici.docsources.domain.SearchFilter.SearchType;
 
-/**
- * 
- * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
- *
- */
+ /**
+  * 
+  * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
+  *
+  */
 public class PaginationFilter {
-    private Integer firstRecord;
-    private Integer length;
-
+	private Integer firstRecord;
+	private Integer length;
+	private SearchType searchType;
+	private Integer sortingColumn;
+	
 	private List<SortingCriteria> sortingCriterias;
-    private Long total;
-    
-    /**
-     * 
-     * @param firstRecord
-     * @param length
-     */
-    public PaginationFilter(Integer firstRecord, Integer length) {
-        this.firstRecord = firstRecord;
-        this.length = length;
+	
+	private String sortingDirection;
+	
+	private Long total;
 
-        this.sortingCriterias = new ArrayList<SortingCriteria>();
-    }
+ 	/**
+ 	 * 
+ 	 * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
+ 	 *
+ 	 */
+ 	public static enum Order {
+ 		ASC("ascending"), DESC("descending");
+         
+ 		private final String order;
 
-    /**
-     * 
-     * @param firstRecord
-     * @param length
-     * @param total
-     */
-    public PaginationFilter(Integer firstRecord, Integer length, Long total) {
-        this.firstRecord = (firstRecord == null) ? new Integer(1) : firstRecord;
-        this.length = length;
-        this.total = total;
+ 	    private Order(String value) {
+ 	    	if (!ObjectUtils.toString(value).equals("")) {
+ 	    		order = value.toUpperCase();
+ 	    	} else {
+ 	    		order = "ASC";
+ 	    	}
+ 	    }
 
-        this.sortingCriterias = new ArrayList<SortingCriteria>();
-    }
+ 	    @Override
+ 	    public String toString(){
+ 	        return order;
+ 	    }
+ 	    
+ 	    public String toStringJPQL(){
+ 	    	if (!ObjectUtils.toString(order).equals("")) {
+ 	    		if (order.equalsIgnoreCase("ascending")) {
+ 	    			return "ASC";
+ 	    		} else {
+ 	    			return "DESC";
+ 	    		}
+ 	    	}
+ 	        return "";
+ 	    }
+ 	}
+ 	/**
+ 	 * 
+ 	 * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
+ 	 *
+ 	 */
+     public class SortingCriteria {
+         private String column;
+         private Integer columnType;
+         private Order order;
 
-    /**
-     * 
-     * @param field
-     * @param order
-     * @return
-     */
-    public void addSortingCriteria(String field, String direction) {
-        this.sortingCriterias.add(new SortingCriteria(field, Order.valueOf(direction.toUpperCase())));
-    }
+         public SortingCriteria(String column, Order order) {
+             this.column = column;
+             this.order = order;
+             this.columnType = SortField.STRING;
+         }
 
-    /**
-     * 
-     * @param field
-     * @param order
-     * @return
-     */
-    public void addSortingCriteria(String field, String direction, Integer fieldType) {
-        this.sortingCriterias.add(new SortingCriteria(field, Order.valueOf(direction.toUpperCase()), fieldType));
-    }
-    
-    /**
-	 * @return the firstRecord
-	 */
-	public Integer getFirstRecord() {
-		return firstRecord;
-	}
+         public SortingCriteria(String column, Order order, Integer columnType) {
+             this.column = column;
+             this.order = order;
+             this.columnType = columnType;
+         }
 
-    public Integer getLength() {
-        return length;
-    }
+ 		/**
+ 		 * @return the column
+ 		 */
+         public String getColumn() {
+             return column;
+         }
 
-    public List<SortingCriteria> getSortingCriterias() {
-        return sortingCriterias;
-    }
+ 		/**
+ 		 * @return the columnType
+ 		 */
+ 		public Integer getColumnType() {
+ 			return columnType;
+ 		}
 
-	/**
-	 * @return the total
-	 */
-	public Long getTotal() {
-		return total;
-	}
+ 		/**
+ 		 * @return the order
+ 		 */
+         public Order getOrder() {
+             return order;
+         }
+     }
 
-	/**
-	 * @param total the total to set
-	 */
-	public void setTotal(Long total) {
-		this.total = total;
-	}
+     /**
+      * 
+      * @param firstRecord
+      * @param length
+      */
+     public PaginationFilter(Integer firstRecord, Integer length) {
+         this.firstRecord = firstRecord;
+         this.length = length;
+         this.sortingColumn = -1;
+         this.sortingDirection="";
+         this.sortingCriterias = new ArrayList<SortingCriteria>();
+     }
 
-	/**
-	 * 
-	 * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
-	 *
-	 */
-	public static enum Order {
-		ASC("ascending"), DESC("descending");
-        
-		private final String order;
+     /**
+ 	 * 
+ 	 * @param firstRecord
+ 	 * @param length
+ 	 * @param sortingColumn
+ 	 */
+     public PaginationFilter(Integer firstRecord, Integer length, Integer sortingColumn, String sortingDirection, SearchType searchType) {
+         this.firstRecord = firstRecord;
+         this.length = length;
+         this.setSortingColumn(sortingColumn);
+         this.setSortingDirection(sortingDirection);
+         this.setSearchType(searchType);
+         this.sortingCriterias = new ArrayList<SortingCriteria>();
+     }
 
-	    private Order(String value) {
-	    	if (!ObjectUtils.toString(value).equals("")) {
-	    		order = value.toUpperCase();
-	    	} else {
-	    		order = "ASC";
-	    	}
-	    }
+     /**
+      * 
+      * @param firstRecord
+      * @param length
+      * @param total
+      */
+     public PaginationFilter(Integer firstRecord, Integer length, Long total) {
+         this.firstRecord = (firstRecord == null) ? new Integer(1) : firstRecord;
+         this.length = length;
+         this.total = total;
 
-	    @Override
-	    public String toString(){
-	        return order;
-	    }
-	    
-	    public String toStringJPQL(){
-	    	if (!ObjectUtils.toString(order).equals("")) {
-	    		if (order.equalsIgnoreCase("ascending")) {
-	    			return "ASC";
-	    		} else {
-	    			return "DESC";
-	    		}
-	    	}
-	        return "";
-	    }
-	}
+         this.sortingCriterias = new ArrayList<SortingCriteria>();
+     }
+     
+     /**
+      * 
+      * @param field
+      * @param order
+      * @return
+      */
+     public void addSortingCriteria(String field, String direction) {
+         this.sortingCriterias.add(new SortingCriteria(field, Order.valueOf(direction.toUpperCase())));
+     }
 
-	/**
-	 * 
-	 * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
-	 *
-	 */
-    public class SortingCriteria {
-        private String column;
-        private Integer columnType;
-        private Order order;
+     /**
+      * 
+      * @param field
+      * @param order
+      * @return
+      */
+     public void addSortingCriteria(String field, String direction, Integer fieldType) {
+         this.sortingCriterias.add(new SortingCriteria(field, Order.valueOf(direction.toUpperCase()), fieldType));
+     }
 
-        public SortingCriteria(String column, Order order) {
-            this.column = column;
-            this.order = order;
-            this.columnType = SortField.STRING;
-        }
+     /**
+ 	 * @return the firstRecord
+ 	 */
+ 	public Integer getFirstRecord() {
+ 		return firstRecord;
+ 	}
 
-        public SortingCriteria(String column, Order order, Integer columnType) {
-            this.column = column;
-            this.order = order;
-            this.columnType = columnType;
-        }
+ 	public Integer getLength() {
+         return length;
+     }
 
-		/**
-		 * @return the column
-		 */
-        public String getColumn() {
-            return column;
-        }
+ 	/**
+ 	 * @return the searchType
+ 	 */
+ 	public SearchType getSearchType() {
+ 		return searchType;
+ 	}
 
-		/**
-		 * @return the order
-		 */
-        public Order getOrder() {
-            return order;
-        }
+ 	/**
+ 	 * @return the sortingColumn
+ 	 */
+ 	public Integer getSortingColumn() {
+ 		return sortingColumn;
+ 	}
 
-		/**
-		 * @return the columnType
-		 */
-		public Integer getColumnType() {
-			return columnType;
-		}
-    }
-}
+ 	public List<SortingCriteria> getSortingCriterias() {
+         return sortingCriterias;
+     }
+
+ 	/**
+ 	 * @return the sortingDirection
+ 	 */
+ 	public String getSortingDirection() {
+ 		return sortingDirection;
+ 	}
+
+ 	/**
+ 	 * @return the total
+ 	 */
+ 	public Long getTotal() {
+ 		return total;
+ 	}
+
+ 	/**
+ 	 * @param searchType the searchType to set
+ 	 */
+ 	public void setSearchType(SearchType searchType) {
+ 		this.searchType = searchType;
+ 	}
+
+ 	/**
+ 	 * @param sortingColumn the sortingColumn to set
+ 	 */
+ 	public void setSortingColumn(Integer sortingColumn) {
+ 		this.sortingColumn = sortingColumn;
+ 	}
+
+ 	/**
+ 	 * @param sortingDirection the sortingDirection to set
+ 	 */
+ 	public void setSortingDirection(String sortingDirection) {
+ 		this.sortingDirection = sortingDirection;
+ 	}
+     /**
+ 	 * @param total the total to set
+ 	 */
+ 	public void setTotal(Long total) {
+ 		this.total = total;
+ 	}
+ }
