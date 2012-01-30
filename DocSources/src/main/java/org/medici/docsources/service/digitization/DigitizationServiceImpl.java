f0d50b6911fbdac88aae040792a525011cdbcf43
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2012-01-30 21:49:35 +0000

    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@1515 3b25252b-017b-4ae5-8f61-fa87a855a72e

---
+++ DocSources/src/main/java/org/medici/docsources/service/digitization/DigitizationServiceImpl.java
@@ -0,0 +1,88 @@
+/*
+ * DigitizationServiceImpl.java
+ * 
+ * Developed by Medici Archive Project (2010-2012).
+ * 
+ * This file is part of DocSources.
+ * 
+ * DocSources is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * DocSources is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, if you link this library with other files to
+ * produce an executable, this library does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * This exception does not however invalidate any other reasons why the
+ * executable file might be covered by the GNU General Public License.
+ */
+package org.medici.docsources.service.digitization;
+
+import org.medici.docsources.dao.catalog.CatalogDAO;
+import org.medici.docsources.domain.Catalog;
+import org.medici.docsources.exception.ApplicationThrowable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * 
+ * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
+ */
+@Service
+@Transactional(readOnly=true)
+public class DigitizationServiceImpl implements DigitizationService {
+	@Autowired
+	private CatalogDAO catalogDAO;
+
+	/**
+	 * 
+	 */
+	public Catalog findCatalog(Integer catalogId) {
+		// TODO Auto-generated method stub
+		return new Catalog();
+	}
+
+	/**
+	 * @return the catalogDAO
+	 */
+	public CatalogDAO getCatalogDAO() {
+		return catalogDAO;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Transactional(readOnly=false, propagation=Propagation.REQUIRED)
+	@Override
+	public void save(Catalog catalog) throws ApplicationThrowable {
+		try {
+			if ((catalog.getId() != null) && (catalog.getId() > 0)) {
+				getCatalogDAO().persist(catalog);
+			} else {
+				getCatalogDAO().merge(catalog);
+			}
+				
+		}catch (Throwable th) {
+			throw new ApplicationThrowable(th);
+		}
+	}
+
+	/**
+	 * @param catalogDAO the catalogDAO to set
+	 */
+	public void setCatalogDAO(CatalogDAO catalogDAO) {
+		this.catalogDAO = catalogDAO;
+	}
+
+}
---
+++ DocSources/src/main/java/org/medici/docsources/validator/digitization/EditCatalogRequestValidator.java
@@ -0,0 +1,107 @@
+/*
+ * EditCatalogRequestValidator.java
+ * 
+ * Developed by Medici Archive Project (2010-2012).
+ * 
+ * This file is part of DocSources.
+ * 
+ * DocSources is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * DocSources is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, if you link this library with other files to
+ * produce an executable, this library does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * This exception does not however invalidate any other reasons why the
+ * executable file might be covered by the GNU General Public License.
+ */
+package org.medici.docsources.validator.digitization;
+
+import org.medici.docsources.command.digitization.EditCatalogCommand;
+import org.medici.docsources.service.digitization.DigitizationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+/**
+ * Validator bean for action "Edit Catalog".
+ * 
+ * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
+ * 
+ */
+public class EditCatalogRequestValidator implements Validator {
+	@Autowired
+	private DigitizationService catalogService;
+
+	/**
+	 * @return the catalogService
+	 */
+	public DigitizationService getCatalogService() {
+		return catalogService;
+	}
+
+	/**
+	 * @param catalogService the catalogService to set
+	 */
+	public void setCatalogService(DigitizationService catalogService) {
+		this.catalogService = catalogService;
+	}
+
+	/**
+	 * Indicates whether the given class is supported by this converter. This
+	 * validator supports only ModifyDocumentCommand.
+	 * 
+	 * @param givenClass the class to test for support
+	 * @return true if supported; false otherwise
+	 */
+	@SuppressWarnings("rawtypes")
+	public boolean supports(Class givenClass) {
+		return givenClass.equals(EditCatalogCommand.class);
+	}
+
+	/**
+	 * Validate the supplied target object, which must be of a Class for which
+	 * the supports(Class) method typically has (or would) return true. The
+	 * supplied errors instance can be used to report any resulting validation
+	 * errors.
+	 * 
+	 * @param object the object that is to be validated (can be null)
+	 * @param errors contextual state about the validation process (never null)
+	 */
+	public void validate(Object object, Errors errors) {
+		EditCatalogCommand editCatalogCommand = (EditCatalogCommand) object;
+		editCatalogCommand.getId();
+		// TODO : implement validation logic
+		
+	}
+
+
+	/**
+	 * 
+	 * @param catalogId
+	 * @param errors
+	 */
+	public void validateCatalogId(Integer catalogId, Errors errors) {
+		if (errors.hasErrors()) {
+			return;
+		}
+		
+		if (catalogId != null) {
+			if (catalogId > 0) {
+				if (getCatalogService().findCatalog(catalogId) == null) {
+					errors.reject("catalogId", "error.catalogId.notfound");
+				}
+			}
+		}
+	}
+}
