ef3adfe344ad8935602f2332463af6fa6a65fd5c
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2012-02-01 23:37:47 +0000

    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@1529 3b25252b-017b-4ae5-8f61-fa87a855a72e

--- DocSources/src/main/java/org/medici/docsources/controller/peoplebase/ShowRoleCatPeoplePersonController.java
+++ DocSources/src/main/java/org/medici/docsources/controller/peoplebase/ShowRoleCatPeoplePersonController.java
@@ -48,7 +48,7 @@ import org.springframework.web.servlet.ModelAndView;
  * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
  */
 @Controller
-@RequestMapping("/de/peoplebase/ShowRoleCatPeoplePerson")
+@RequestMapping("/src/peoplebase/ShowRoleCatPeoplePerson")
 public class ShowRoleCatPeoplePersonController {
 	@Autowired
 	private PeopleBaseService peopleBaseService;
--- DocSources/src/main/java/org/medici/docsources/controller/peoplebase/ShowTitlesOrOccupationsPeoplePersonController.java
+++ DocSources/src/main/java/org/medici/docsources/controller/peoplebase/ShowTitlesOrOccupationsPeoplePersonController.java
@@ -48,7 +48,7 @@ import org.springframework.web.servlet.ModelAndView;
  * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
  */
 @Controller
-@RequestMapping("/de/peoplebase/ShowTitlesOrOccupationsPeoplePerson")
+@RequestMapping("/src/peoplebase/ShowTitlesOrOccupationsPeoplePerson")
 public class ShowTitlesOrOccupationsPeoplePersonController {
 	@Autowired
 	private PeopleBaseService peopleBaseService;
