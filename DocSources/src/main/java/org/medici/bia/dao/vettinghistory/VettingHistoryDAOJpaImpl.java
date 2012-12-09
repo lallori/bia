9e19767166c792d97a93402b7150a51ae29b14b2
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2012-12-09 11:53:25 +0000

    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@2816 3b25252b-017b-4ae5-8f61-fa87a855a72e

---
+++ DocSources/src/main/java/org/medici/bia/dao/vettinghistory/VettingHistoryDAOJpaImpl.java
@@ -0,0 +1,134 @@
+/*
+ * VettingHistoryDAOJpaImpl.java
+ * 
+ * Developed by Medici Archive Project (2010-2012).
+ * 
+ * This file is part of DocSources.
+ * 
+ * DocSources is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * DocSources is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * As a special exception, if you link this library with other files to
+ * produce an executable, this library does not by itself cause the
+ * resulting executable to be covered by the GNU General Public License.
+ * This exception does not however invalidate any other reasons why the
+ * executable file might be covered by the GNU General Public License.
+ */
+package org.medici.bia.dao.vettinghistory;
+
+import org.medici.bia.dao.JpaDao;
+import org.medici.bia.dao.document.DocumentDAO;
+import org.medici.bia.dao.people.PeopleDAO;
+import org.medici.bia.dao.place.PlaceDAO;
+import org.medici.bia.dao.volume.VolumeDAO;
+import org.medici.bia.domain.VettingHistory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <b>VettingHistoryDAOJpaImpl</b> is a default implementation of
+ * <b>VettingHistoryDAO</b>.
+ * 
+ * @author Lorenzo Pasquinelli (<a href=mailto:l.pasquinelli@gmail.com>l.pasquinelli@gmail.com</a>)
+ * @author Matteo Doni (<a href=mailto:donimatteo@gmail.com>donimatteo@gmail.com</a>)
+ * 
+ * @see org.medici.bia.domain.UserHistory
+ */
+@Repository
+public class VettingHistoryDAOJpaImpl extends JpaDao<Integer, VettingHistory> implements VettingHistoryDAO {
+	/**
+	 * 
+	 *  If a serializable class does not explicitly declare a serialVersionUID, 
+	 *  then the serialization runtime will calculate a default serialVersionUID 
+	 *  value for that class based on various aspects of the class, as described
+	 *  in the Java(TM) Object Serialization Specification. However, it is 
+	 *  strongly recommended that all serializable classes explicitly declare 
+	 *  serialVersionUID values, since the default serialVersionUID computation 
+	 *  is highly sensitive to class details that may vary depending on compiler
+	 *  implementations, and can thus result in unexpected 
+	 *  InvalidClassExceptions during deserialization. Therefore, to guarantee a
+	 *   consistent serialVersionUID value across different java compiler 
+	 *   implementations, a serializable class must declare an explicit 
+	 *  serialVersionUID value. It is also strongly advised that explicit 
+	 *  serialVersionUID declarations use the private modifier where possible, 
+	 *  since such declarations apply only to the immediately declaring 
+	 *  class--serialVersionUID fields are not useful as inherited members. 
+	 */
+	private static final long serialVersionUID = -1852942809900025399L;
+
+	@Autowired
+	private DocumentDAO documentDAO;
+	@Autowired
+	private PeopleDAO peopleDAO;
+	@Autowired
+	private PlaceDAO placeDAO;
+	@Autowired
+	private VolumeDAO volumeDAO;
+
+	/**
+	 * @return the documentDAO
+	 */
+	public DocumentDAO getDocumentDAO() {
+		return documentDAO;
+	}
+
+	/**
+	 * @return the peopleDAO
+	 */
+	public PeopleDAO getPeopleDAO() {
+		return peopleDAO;
+	}
+
+	/**
+	 * @return the placeDAO
+	 */
+	public PlaceDAO getPlaceDAO() {
+		return placeDAO;
+	}
+
+	/**
+	 * @return the volumeDAO
+	 */
+	public VolumeDAO getVolumeDAO() {
+		return volumeDAO;
+	}
+
+	/**
+	 * @param documentDAO the documentDAO to set
+	 */
+	public void setDocumentDAO(DocumentDAO documentDAO) {
+		this.documentDAO = documentDAO;
+	}
+
+	/**
+	 * @param peopleDAO the peopleDAO to set
+	 */
+	public void setPeopleDAO(PeopleDAO peopleDAO) {
+		this.peopleDAO = peopleDAO;
+	}
+
+	/**
+	 * @param placeDAO the placeDAO to set
+	 */
+	public void setPlaceDAO(PlaceDAO placeDAO) {
+		this.placeDAO = placeDAO;
+	}
+
+	/**
+	 * @param volumeDAO the volumeDAO to set
+	 */
+	public void setVolumeDAO(VolumeDAO volumeDAO) {
+		this.volumeDAO = volumeDAO;
+	}
+}
