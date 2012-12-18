528806faf3d6a07b658d7c4beb0d49dcd951d24c
pasquinelli <pasquinelli@3b25252b-017b-4ae5-8f61-fa87a855a72e>
2012-12-18 23:24:14 +0000

    git-svn-id: https://svn.java.net/svn/docsources~svn/trunk@2867 3b25252b-017b-4ae5-8f61-fa87a855a72e

--- DocSources/src/main/java/org/medici/bia/controller/community/AjaxController.java
+++ DocSources/src/main/java/org/medici/bia/controller/community/AjaxController.java
@@ -52,8 +52,6 @@ import org.medici.bia.service.community.CommunityService;
 import org.medici.bia.service.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
